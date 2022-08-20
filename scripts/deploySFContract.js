a99129b31b61d16a0a9ce94ecf79423c53f498c7
Hunter King <hking6776@gmail.com>
2022-08-20 14:21:28 -0500

    superfluid functional

---
+++ scripts/deploySFContract.js
@@ -0,0 +1,29 @@
+const hre = require("hardhat");
+const { Framework } = require("@superfluid-finance/sdk-core");
+require('dotenv').config();
+
+const owner = process.env.PUB_KEY;
+
+// run: npx hardhat run scripts/deploySFContract.js --network matic
+async function main() {
+  const provider = new hre.ethers.providers.JsonRpcProvider(process.env.MATIC_URL);
+
+  const sf = await Framework.create({
+    chainId: (await provider.getNetwork()).chainId,
+    provider,
+    customSubgraphQueriesEndpoint: "",
+    dataMode: "WEB3_ONLY"
+  });
+
+  const Router = await hre.ethers.getContractFactory("SFRouter");
+  const sfRouter = await Router.deploy(sf.settings.config.hostAddress, owner);
+
+  await sfRouter.deployed();
+
+  console.log(`\n\ncontract deployed at: ${sfRouter.address}\n`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
