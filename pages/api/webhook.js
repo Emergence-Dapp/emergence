3b66c2ee4719d760abb694e6bb6fb6b524eb95dc
Anuark <jaicof@gmail.com>
2022-08-21 00:54:03 -0500

    webhook update for recording

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,52 +1,45 @@
 import aws from 'aws-sdk';
+import axios from 'axios';
+var jwt = require('jsonwebtoken');
+var uuid4 = require('uuid4');
 
-aws.config.update({
-  accessKeyId: process.env.ACCESS_KEY,
-  secretAccessKey: process.env.SECRET_KEY,
-  region: 'us-east-2',
-  signatureVersion: 'v4',
-});
+var app_access_key = '62f6449dc16640065697182c';
+var app_secret = 'ojJPdgjgjI4DMT4btFGjGazgighCn4rBhiNh9XvpKgpW8ZP4aa7XGEtOF35OTzounnQDqekMiyXAfBXh1BfgQg1zGZR6HvKd4OdrnqP1u4Q2VVew6UbmaER9I9G1cssLb5mhNjeap8420nWP5TghtdjyD4t4wQ3EO8Z5JUGdQno=';
+
+const payload = {
+    access_key: app_access_key,
+    type: 'management',
+    version: 2,
+    iat: Math.floor(Date.now() / 1000),
+    nbf: Math.floor(Date.now() / 1000)
+};
+
+const getManagementKey = () => {
+    return Promise((resolve, reject) => {
+        jwt.sign(
+            payload,
+            app_secret,
+            {
+                algorithm: 'HS256',
+                expiresIn: '24h',
+                jwtid: uuid4()
+            },
+            function(err, token) {
+                resolve(token);
+            }
+        );
+    });
+}
+
+
 
 export default async function handler(req, res) {
-    // const s3Client = new aws.S3();
-
-    // const saveTranscriptions = (data) => {
-    //     return new Promise((resolve, reject) => {
-    //         s3Client.putObject({
-    //             Bucket: 'emergence-dapp',
-    //             Key: 'transcriptions.json',
-    //             Body: data
-    //         }, (err, res) => {
-    //             if (err) {
-    //                 return reject(err);
-    //             }
-    //             resolve(res);
-    //         })
-    //     })
-    // };
-
-    // const getTranscriptions = () => {
-    //     return new Promise((resolve, reject) => {
-    //         const searchParams = {
-    //             Bucket: 'emergence-dapp',
-    //             Key: 'transcriptions.json'
-    //         };
-
-    //         s3Client.getObject(searchParams, (err, data) => {
-    //             if (err) {
-    //                 return reject(err);
-    //             }
-    //             resolve(data.Body.toString());
-    //         });
-    //     });
-    // };
-
     const {
         body: { type, data },
         method
     } = req
 
-    console.log({ method, type, data });
+    console.log({ method, type });
     if (method == 'POST') {
         if (type == 'recording.success') {
             const videoUrl = data.URL;
@@ -57,26 +50,47 @@ export default async function handler(req, res) {
             console.log('recording.failed');
             console.log({ data });
             return res.status(200).send('success');
+        } else if (type == 'session.open.success') {
+            const authKey = getManagementKey()
+            const options = {
+                method: 'POST',
+                url: 'https://prod-in2.100ms.live/api/v2/beam',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: 'Bearer ' + authKey
+                },
+                data: {
+                    operation: 'start',
+                    room_id: '63004db6b1e780e78c3bc4ff',
+                    meeting_url: 'https://video-demo-next.app.100ms.live/preview/63004db6b1e780e78c3bc4ff/stage?skip_preview=true',
+                    record: true,
+                    resolution: { width: 1280, height: 720 }
+                }
+            };
+
+            const resp = await axios.request(options);
+            console.log({ data: resp.data });
+            return res.status(200).send('meeting started');
         } else if (type == 'session.close.success') {
-            // let indexData = '[]';
-            // try {
-            //     indexData = await getTranscriptions()
-            //     console.log('getTranscriptions results', indexData);
-            // } catch (err) {
-            //     console.error('getTranscriptions failed');
-            //     console.error({ err })
-            // }
+            const authKey = getManagementKey()
+            const options = {
+                method: 'POST',
+                url: 'https://prod-in2.100ms.live/api/v2/beam',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: 'Bearer ' + authKey
+                },
+                data: {
+                    operation: 'stop',
+                    room_id: '63004db6b1e780e78c3bc4ff',
+                }
+            };
 
-            // const indexDataObj = JSON.parse(indexData);
-            // indexDataObj.push({ ...data });
-            // try {
-            //     await saveTranscriptions(JSON.stringify(indexDataObj));
-            // } catch (err) {
-            //     console.error('saveTranscriptions failed');
-            //     console.error(err);
-            // }
-
-            // return res.status(200).json(indexDataObj);
+            const resp = await axios.request(options);
+            console.log({ data: resp.data });
+            return res.status(200).send('meeting stoped');
+        } else if (type == 'beam.recording.success') {
+            console.log({ location: data.location });
         }
     }
 
